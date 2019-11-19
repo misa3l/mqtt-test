fd702229972c17c5f9a4cf65d3b8370b3aa2e05d
misa3l <blacksecured@gmail.com>
2019-11-19 00:57:23 -0400

    Init...

---
+++ subscribe.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as mqtt
+import configparser
+import zlib
+import binascii
+import json
+import uuid
+import sys
+
+#### Configuration #####
+config = configparser.ConfigParser()
+config.read(sys.path[0]+'/configuracion.ini')
+#broker_host	= str(config['MQTT']['broker_host'])
+broker_host	= str(config['MQTT']['broker_host'])
+broker_port	= int(config['MQTT']['broker_port'])
+#username	= str(config['MQTT']['mqtt_user'])
+#password	= str(config['MQTT']['mqtt_pass'])
+mqtt_qos	= int(config['MQTT']['mqtt_qos'])
+keep_alive	= int(config['MQTT']['mqtt_keep_alive'])
+debug		= bool(config['DEV']['debug'])
+client_id = "mqtt-subscribe-{0}".format(uuid.uuid4().hex[:8])
+#client_id = 'mqtt-subscribe-' + str(uuid.uuid4())
+#### Configuration #####
+
+def on_connect(client, userdata, flags, rc):
+	if rc==0:
+		print("connection OK rc={}".format(str(rc)))
+	else:
+		print("Bad connection Returned code={}".format(str(rc)))
+	
+	# subscribe to topic
+	client.subscribe("hermes/temp/in", qos=mqtt_qos)
+	
+	#client.subscribe('hermes/hotword/default/detected')
+	#client.subscribe('hermes/intent/INTENT_NAME')
+
+
+def on_message(client, userdata, msg):
+	# decompress data
+	hex_msg = msg.payload.decode('utf-8')
+	bin_data = binascii.unhexlify(hex_msg)
+	json_data = zlib.decompress(bin_data)
+	
+	#debug
+	if debug==True:
+		print("Recv: {} ret: {} qos: {}".format(str(json_data), str(msg.retain), str(msg.qos)))
+	
+	# load json data
+	parsed_json = json.loads(json_data)
+	
+	#if msg.retain==1:
+	#	print("This is a retained message")
+	
+	if parsed_json['temperature'] >= 23:
+		print("Recv:{} High {} ret:{} qos:{}".format(str(msg.topic), str(parsed_json['temperature']), str(msg.retain), str(msg.qos)))
+		
+		# data to send
+		json_data = {'msg':"High",'Temp':parsed_json['temperature']}
+		data_string = json.dumps(json_data)
+		
+		# compress data
+		compressed = zlib.compress(data_string.encode("utf-8"))
+		hex_data = binascii.hexlify(compressed)
+		
+		#debug
+		if debug==True:
+			print("Send: {}".format(str(hex_data)))
+		
+		# send data to topic
+		client.publish("hermes/temp/out", hex_data, qos=mqtt_qos, retain=True)
+		
+	#if msg.topic == 'hermes/hotword/default/detected':
+	#	print("Hotword detected!")
+	#	if msg.payload.decode()=="1":
+	#		print("Mensaje 1")
+	#elif msg.topic == 'hermes/intent/INTENT_NAME':
+	#	print("Intent detected!")
+	#	if msg.payload.decode()=="2":
+	#		print("Mensaje 2")
+
+
+def on_publish(client, obj, mid):
+    #print("mid: " + str(mid))
+    pass
+
+
+def on_subscribe(client, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(client, obj, level, string):
+    print(string)
+
+
+#### Connection #####
+client = mqtt.Client(client_id, clean_session=False)
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_publish = on_publish
+client.on_subscribe = on_subscribe
+#client.on_log = on_log
+#client.username_pw_set(username, password)
+client.connect(broker_host, broker_port, keep_alive)
+client.loop_forever()
+#### Connection #####
